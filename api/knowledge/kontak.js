c9e800890c7b9380854baff87f6839ff57a6f908
prabowo-gibran-02-omke-gams <zakkyilhamfirmansyahhh@gmail.com>
2026-07-26 16:06:55 +0700

    toll

--- api/knowledge/kontak.js
+++ api/knowledge/kontak.js
@@ -15,5 +15,5 @@ Jika ada yang bertanya tentang alamat sekolah jawab aja alamat sekolah smk bahru
 Kelurahan Putat Jaya, Kecamatan Sawahan
 Kota Surabaya, Jawa Timur
 Kode Pos 60255 dan jika ada yang minta email sekolah berikan smk.bahrululum@gmail.com(hanya jika ada yang minta email sekolah) dan berikan nomer telpon resmi sekolah jika ada yang minta nomer telpon sekolah berikut nomernya (031) 9953 9925 suruh mereka hubungi nomer tersebut, 
-dan jika berikan saran sedikit atau kasih promosi diakhir teks untuk mengikuti instagram sekolah (https://instagram.com/smkbu.sby @smkbu.sby) itu adalah instagram resmi sekolah dan buat teks semenarik mungkin agar user tertarik melihat instagram sekolah kita dan berikan emoji disetiap akhir kalimat.`
+dan jika berikan saran sedikit atau kasih promosi diakhir teks untuk mengikuti instagram sekolah dan jika ada yang minta instagram sekolah kasih aja ini (https://instagram.com/smkbu.sby @smkbu.sby) itu adalah instagram resmi sekolah dan buat teks semenarik mungkin agar user tertarik melihat instagram sekolah kita dan berikan emoji disetiap akhir kalimat.`
 }
