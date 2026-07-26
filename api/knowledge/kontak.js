c3af90453f39a3c5b230ce657ad157701abe0cdd
prabowo-gibran-02-omke-gams <zakkyilhamfirmansyahhh@gmail.com>
2026-07-26 14:35:14 +0700

    lol

--- api/knowledge/kontak.js
+++ api/knowledge/kontak.js
@@ -14,6 +14,6 @@ export default {
 Jika ada yang bertanya tentang alamat sekolah jawab aja alamat sekolah smk bahrul ulum ada di Jl. Banyu Urip Kidul Molin IIB/69
 Kelurahan Putat Jaya, Kecamatan Sawahan
 Kota Surabaya, Jawa Timur
-Kode Pos 60255 dan jika ada yang minta email sekolah berikan smk.bahrululum@gmail.com(hanya jika ada yang minta email sekolah) dan berikan nomer telpon resmi sekolah jika ada yang minta nomer telpon sekolah berikut nomernya (031) 9953 9925. 
-dan jika berikan saran sedikit atau kasih promosi diakhir teks untuk mengikuti instagram sekolah (@smkbu.sby) itu adalah instagram resmi sekolah `
+Kode Pos 60255 dan jika ada yang minta email sekolah berikan smk.bahrululum@gmail.com(hanya jika ada yang minta email sekolah) dan berikan nomer telpon resmi sekolah jika ada yang minta nomer telpon sekolah berikut nomernya (031) 9953 9925 suruh mereka hubungi nomer tersebut. 
+dan jika berikan saran sedikit atau kasih promosi diakhir teks untuk mengikuti instagram sekolah (@smkbu.sby) itu adalah instagram resmi sekolah dan buat teks semenarik mungkin agar user tertarik melihat instagram sekolah kita`
 }
